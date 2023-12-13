e16f0861e86f98aae0a0343814498bfea76c934e
Kennedy Mac <kennedy.adams@dcmail.ca>
2023-12-12 23:24:13 -0500

    there

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import { ServicesList } from "./services";
 import { Link } from "react-router-dom";
+import logo from '../images/logo.png';
 
 import hero from '../images/hero.jpg';
 
@@ -7,32 +8,30 @@ import hero from '../images/hero.jpg';
 const Home = () => {
     return (
         <>
-            <h1>Radiant Realms</h1>
 
             <section className="hero">
                 <img src={hero} alt="" />
+                <h1>Welcome to Radiant Realms</h1>
 
             </section>
 
-
-            {/* <section className="featuredServices">
-                <h2> featured services</h2>
-                    <ServicesList />
-                    <Link to="/services">view all services</Link>
-            </section> */}
-
-            <section className="card">
+            <section className="card homeCard">
                 <h2>Understanding Mental Health</h2>
-                <h3>What is Mental Health?</h3>
-                <p><strong>Mental health is more than the absence of mental illness. </strong>It encompasses a state of emotional, psychological, and social well-being. Good mental health enables individuals to handle stress, form meaningful relationships, and make decisions.</p>
-                <h3>Seeking Help is a Sign of Strength</h3>
-                <p><strong>Everyone faces challenges, and seeking help is a sign of strength, not weakness. </strong> If you or someone you know is experiencing mental health difficulties, reaching out to a mental health professional can make a significant difference.</p>
-                <h3>Mental Health and Everyday Life</h3>
-                <p><strong>Small changes can have a big impact on mental well-being.</strong>Incorporating activities like regular exercise, maintaining social connections, and practicing mindfulness can contribute to a positive mental health routine.</p>
-
+                <article>
+                    <h3>What is Mental Health?</h3>
+                    <p><strong>Mental health is more than the absence of mental illness. </strong>It encompasses a state of emotional, psychological, and social well-being. Good mental health enables individuals to handle stress, form meaningful relationships, and make decisions.</p>
+                </article>
+                <article>
+                    <h3>Seeking Help is a Sign of Strength</h3>
+                    <p><strong>Everyone faces challenges, and seeking help is a sign of strength, not weakness. </strong> If you or someone you know is experiencing mental health difficulties, reaching out to a mental health professional can make a significant difference.</p>
+                </article>
+                <article>
+                    <h3>Mental Health and Everyday Life</h3>
+                    <p><strong>Small changes can have a big impact on mental well-being.</strong>Incorporating activities like regular exercise, maintaining social connections, and practicing mindfulness can contribute to a positive mental health routine.</p>
+                </article>
             </section>
 
-            <section className="card">
+            <section className="card homeCard">
                 <h2>About Radiant Realms</h2>
                 <p>Welcome to Radiant Realms, a sanctuary for mental well-being and personal growth. At Radiant Realms, we believe in the transformative power of inner peace and the journey towards a balanced, resilient mind.</p>
                 <article>
@@ -43,6 +42,11 @@ const Home = () => {
                     <h3>Who We Are</h3>
                     <p>Radiant Realms is more than just a therapy center; we are a community of compassionate therapists dedicated to guiding you on your path to radiant living. Our diverse team brings a wealth of experience and a commitment to providing personalized care that meets your unique needs.</p>
                 </article>
+              
+                <article>
+                    <h3>Our Commitment</h3>
+                    <p>We are committed to being a guiding light on your path to mental wellness. Whether you're navigating life's challenges, seeking personal growth, or addressing specific issues, Radiant Realms is here for you.</p>
+                </article>
                 <article>
                     <h3>What Sets Us Apart</h3>
                     <ul>
@@ -50,10 +54,6 @@ const Home = () => {
                         <li><strong>Tailored Care</strong> Your journey is unique, and so is our approach. Our therapists work collaboratively with you, using evidence-based practices to foster growth and resilience.</li>
                         <li><strong>Safe and Inclusive Space: </strong> Radiant Realms is a safe haven for everyone. We celebrate diversity and create an inclusive environment where all individuals feel welcomed and supported.</li>
                     </ul>
-                </article>
-                <article>
-                    <h3>Our Commitment</h3>
-                    <p>We are committed to being a guiding light on your path to mental wellness. Whether you're navigating life's challenges, seeking personal growth, or addressing specific issues, Radiant Realms is here for you.</p>
                 </article>
             </section>
 
